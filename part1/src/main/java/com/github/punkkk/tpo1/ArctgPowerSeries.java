f268033b9c58f769a44418dd809ce04f7a3c99f1
Victor Kozlov <punkrus18@gmail.com>
2017-03-12 19:42:57 +0300

    second-variant delete comments; in-row vars init;

--- part1/src/main/java/com/github/punkkk/tpo1/ArctgPowerSeries.java
+++ part1/src/main/java/com/github/punkkk/tpo1/ArctgPowerSeries.java
@@ -1,6 +1,6 @@
 package com.github.punkkk.tpo1;
 
-public class ArctgPowerSeries
+  class ArctgPowerSeries
 {
     private Double getSeriesMember(Double x, int n){
         Double dividend = Math.pow(-1, ( n - 1 )) * Math.pow(x, ( 2 * n - 1));
@@ -10,12 +10,9 @@ public class ArctgPowerSeries
     public Double getArctg(Double value, Double accuracy){
         Expect.notNullArgument(value, "value", "ArctgPowerSeries.getArctg value can not be null");
         Expect.notNullArgument(accuracy, "accuracy", "ArctgPowerSeries.getArctg accuracy can not be null");
-        Double result = 0.;
-        Double newMember = 0.;
-//        Double oldResult = Double.MAX_VALUE;
+        Double newMember, result = 0.;
         for (int i = 1; Math.abs(newMember = this.getSeriesMember(value, i)) > accuracy; i++) {
             result += newMember;
-//            result += this.getSeriesMember(value, i);
         }
         return result;
     }
